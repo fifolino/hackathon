de6858d84200d2cb6e8974321a9e22201293b3ba
alexia <alexia.martinez-lee@epitech.eu>
2023-10-14 19:22:38 +0200

    init connexion page + add logo

---
+++ app/lib/screen/ConnexionScreen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class ConnexionScreen extends StatefulWidget {
+  const ConnexionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ConnexionScreen> createState() => _ConnexionScreenState();
+}
+
+class _ConnexionScreenState extends State<ConnexionScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0x001a3c8c),
+      body: Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 26, 60, 140),
+                Color.fromARGB(155, 26, 60, 140),
+              ]),
+        ),
+        child: Column(
+          children: [
+            // image logo
+            Container(
+              margin: const EdgeInsets.only(top: 100),
+              child: const Image(
+                image: AssetImage('assets/images/logo.png'),
+                width: 200,
+                height: 200,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Mot de passe',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+                child: const Text('Connexion'),
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
