29591446cf82471851c3df6d6cbedd9c91d1781f
Clarisse Eynard <clarisse.eynard@epitech.eu>
2023-10-14 18:53:54 +0200

    add struct

---
+++ app/lib/screen/HomeScreen.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0x001a3c8c),
+      body: Text('Hello World'),
+      
+    );
+  }
+}
