947d3eba9b08db6d41884f85618790e54d1a9c89
Clarisse Eynard <clarisse.eynard@epitech.eu>
2023-10-15 13:54:07 +0200

    remove banner

--- app/lib/API/request.dart
+++ app/lib/API/request.dart
@@ -6,16 +6,16 @@ class Request {
   final String url = "http://localhost:3001";
 
   Future<bool> login(String email, String mdp) async {
-    // try {
-    //   await http.post(Uri.parse("$url/login"),
-    //       headers: <String, String>{
-    //         'Content-Type': 'application/json; charset=UTF-8',
-    //       },
-    //       body: jsonEncode({"username": email, "password": mdp}));
+    try {
+      await http.post(Uri.parse("$url/login"),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode({"username": email, "password": mdp}));
       return true;
-    // } on HttpException catch (_) {
-    //   return false;
-    // }
+    } on HttpException catch (_) {
+      return false;
+    }
   }
 
   Future<dynamic> getData() async {
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Rénov'ACM",
       theme: ThemeData(
         fontFamily: 'Roboto',
